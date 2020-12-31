d4bce1a32cf6be1066e2f745091e93bde0179ad1
Lavanya Chinta <chintalavanya97@gmail.com>
2020-12-31 18:09:12 +0530

    Add project files.

---
+++ ConsoleGitApp/hello.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGitApp
+{
+    class hello
+    {
+        public void add()
+        {
+            Console.WriteLine("add method called");
+        }
+    }
+}
